194cf19b6bd44cf717702da1d59272afd543e331
Jeongin Lee <jil8885@hanyang.ac.kr>
2023-09-03 19:00:19 +0900

    Initial Commit

---
+++ hyuabot/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hyuabot
+//
+//  Created by 이정인 on 2023/09/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
