7f6a9ecbdbcf8e42374c7c21e5170a0e70da2953
Jeongin Lee <jil8885@hanyang.ac.kr>
2023-09-04 13:52:01 +0900

    feat: Set custom font as default

--- hyuabot/AppDelegate.swift
+++ hyuabot/AppDelegate.swift
@@ -10,6 +10,11 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
+
+    override init() {
+        super.init()
+        UIFont.overrideInitialize()
+    }
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         return true
---
+++ hyuabot/Utility/CustomFont.swift
@@ -0,0 +1,82 @@
+import UIKit
+
+extension UIFont {
+    static func poppins(size: CGFloat, weight: UIFont.Weight) -> UIFont {
+        let familyName = "Poppins"
+        var weightString: String
+
+        switch weight {
+            case .black: weightString = "Black"
+            case .bold: weightString = "Bold"
+            case .heavy: weightString = "ExtraBold"
+            case .ultraLight: weightString = "ExtraLight"
+            case .light: weightString = "Light"
+            case .medium: weightString = "Medium"
+            case .regular: weightString = "Regular"
+            case .semibold: weightString = "SemiBold"
+            case .thin: weightString = "Thin"
+            default: weightString = "Regular"
+        }
+        return UIFont(name: "\(familyName)-\(weightString)", size: size) ?? UIFont.systemFont(ofSize: size, weight: weight)
+    }
+
+    @objc class func mySystemFont(ofSize size: CGFloat) -> UIFont {
+        UIFont.poppins(size: size, weight: .regular)
+    }
+
+    @objc class func myBoldSystemFont(ofSize size: CGFloat) -> UIFont {
+        UIFont.poppins(size: size, weight: .bold)
+    }
+
+    @objc class func myItalicSystemFont(ofSize size: CGFloat) -> UIFont {
+        UIFont.poppins(size: size, weight: .regular)
+    }
+
+    @objc convenience init(myCoder aDecoder: NSCoder) {
+        guard
+            let fontDescriptor = aDecoder.decodeObject(forKey: "UIFontDescriptor") as? UIFontDescriptor,
+            let fontAttribute = fontDescriptor.fontAttributes[.nsctFontUIUsage] as? String
+        else {
+            self.init(myCoder: aDecoder)
+            return
+        }
+
+        var fontName = ""
+        switch fontAttribute {
+            case "CTFontRegularUsage":
+                fontName = "Poppins-Regular"
+            case "CTFontEmphasizedUsage", "CTFontBoldUsage":
+                fontName = "Poppins-Bold"
+            case "CTFontObliqueUsage":
+                fontName = "Poppins-Regular"
+            default:
+                fontName = "Poppins-Regular"
+        }
+
+        self.init(name: fontName, size: fontDescriptor.pointSize)!
+    }
+
+    class func overrideInitialize() {
+        if self == UIFont.self {
+            let systemFontMethod = class_getClassMethod(self, #selector(systemFont(ofSize:)))
+            let mySystemFontMethod = class_getClassMethod(self, #selector(mySystemFont(ofSize:)))
+            method_exchangeImplementations(systemFontMethod!, mySystemFontMethod!)
+
+            let boldSystemFontMethod = class_getClassMethod(self, #selector(boldSystemFont(ofSize:)))
+            let myBoldSystemFontMethod = class_getClassMethod(self, #selector(myBoldSystemFont(ofSize:)))
+            method_exchangeImplementations(boldSystemFontMethod!, myBoldSystemFontMethod!)
+
+            let italicSystemFontMethod = class_getClassMethod(self, #selector(italicSystemFont(ofSize:)))
+            let myItalicSystemFontMethod = class_getClassMethod(self, #selector(myItalicSystemFont(ofSize:)))
+            method_exchangeImplementations(italicSystemFontMethod!, myItalicSystemFontMethod!)
+
+            let initCoderMethod = class_getInstanceMethod(self, #selector(UIFontDescriptor.init(coder:)))
+            let myInitCoderMethod = class_getInstanceMethod(self, #selector(UIFont.init(myCoder:)))
+            method_exchangeImplementations(initCoderMethod!, myInitCoderMethod!)
+        }
+    }
+}
+
+extension UIFontDescriptor.AttributeName {
+    static let nsctFontUIUsage = UIFontDescriptor.AttributeName(rawValue: "NSCTFontUIUsageAttribute")
+}
