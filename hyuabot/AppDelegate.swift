e0708313fdd83607c4bacaeb36b90b8acd57de4c
Jeongin Lee <jil8885@hanyang.ac.kr>
2023-09-04 13:08:50 +0900

    fix: SceneDelegate not found

--- hyuabot/AppDelegate.swift
+++ hyuabot/AppDelegate.swift
@@ -12,11 +12,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        window = UIWindow(frame: UIScreen.main.bounds)
-        window?.makeKeyAndVisible()
-        window?.backgroundColor = .systemBackground
-        window?.rootViewController = ViewController()
-        
         return true
     }
 }
